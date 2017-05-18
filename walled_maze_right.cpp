e3e6789b3b786ba963b2f9afc920d0edb7932dcb
Alex Lee <alexgrlee@gmail.com>
2017-05-19 11:57:26 +1200

    Should function to follow right wall

--- walled_maze_right.cpp
+++ walled_maze_right.cpp
@@ -7,13 +7,12 @@
 
 const int LEFT_SENSOR = 0;
 const int RIGHT_SENSOR = 1;
-//const int MID_SENSOR = 2;
-	
+
 const int LEFT_MOTOR = 1;
 const int RIGHT_MOTOR = 2;
 
 const int DEF_SPD = 50; // Default speed for motors
-//const int THRESHOLD = 80; // Threshold value to allow turning - Change with testing.
+const int THRESHOLD = 90; // Threshold value for proximity to left wall - Change with testing.
 
 double mazeFactor = 0.15; // To be changed with testing.
 
@@ -33,28 +32,24 @@ int main(){
 		//Reading sensors
 		left_reading = read_analog(LEFT_SENSOR); 
 		right_reading = read_analog(RIGHT_SENSOR);
-		mid_reading = read_analog(MID_SENSOR);
 		
 		std::cout << "Left: " << left_reading << " Right: " << right_reading << std::endl; //Printing sensor values.
 		
-		double sensor_difference = (right_reading - left_reading) * mazeFactor;
-		
-		// If the front value is lower than a threshold level.
-		//if(mid_reading < THRESHOLD){
+		double wall_distance = (THRESHOLD - right_reading) * mazeFactor;
+
 			
-			// If left reading is greater (further away), turn left.
-			if(left_reading > right_reading){
+		// If right reading is too great, turn left.
+		if(right_reading > THRESHOLD){
 			
-				set_motor(LEFT_MOTOR, DEF_SPD + 1*sensor_difference);
-				set_motor(RIGHT_MOTOR, DEF_SPD - sensor_difference);
-			}
+			set_motor(LEFT_MOTOR, DEF_SPD + 1*wall_distance);
+			set_motor(RIGHT_MOTOR, DEF_SPD - wall_distance);
+		}
 		
-			// If right reading is greater, turn right.
-			else if(right_reading > left_reading) {			
-				set_motor(LEFT_MOTOR, DEF_SPD - 1*sensor_difference);
-				set_motor(RIGHT_MOTOR, DEF_SPD + sensor_difference);
-			}
-		//}
+		// If right reading too small, turn right.
+		else if(right_reading < THRESHOLD) {			
+			set_motor(LEFT_MOTOR, DEF_SPD - 1*wall_distance);
+			set_motor(RIGHT_MOTOR, DEF_SPD + wall_distance);
+		}
 		
 		// Otherwise, continue forward.
 		else{
