9b34bc854a0852404437cdff93d908eb2acd88f8
Jordan <jordanatararimu@gmail.com>
2017-05-18 16:18:31 +1200

    Potentially improved isOnLine() and bettered comments

--- main.cpp
+++ main.cpp
@@ -32,43 +32,39 @@ void betterstop(int x) {
 
 bool isOnLine(int height) {
 	const int numberCenterPixels = 40;
-        double totalPixelsWhite = 0;
-        for(int i = 0; i < numberCenterPixels; i++) { //80 Pixels in the center are our detector
-			if(get_pixel(height, i+(int)(CAMERA_WIDTH/2)-20, 3) > threshold) {  // Go from -40 from the center to +40 of the center
-				totalPixelsWhite++;
-			} 
-	}
-        return (totalPixelsWhite >  10); // If 55% of the pixels are white, then assume that the line is in the middle
+    double averagePixelWhiteness = 0;
+    for(int i = 0; i < numberCenterPixels; i++)  //80 Pixels in the center are our detector
+		averagePixelWhiteness += get_pixel(height, i+(CAMERA_WIDTH/2)-20, 3) > threshold)   // Go from -20 from the center to +20 of the center
+
+	averagePixelWhiteness /= numberCenterPixels;
+		
+	return (averagePixelWhiteness >= threshold);  //If the value of the average greater than or = to white
 }
 
 int getRightSideErrorSignal() {
-    int TotalRightSidePixels = (int)(CAMERA_WIDTH/2)-40; //The -40 is so it doesnt read the 40 pixels that are used for line detection in the middle
+    const int TotalRightSidePixels = (int)(CAMERA_WIDTH/2)-40; //The -40 is so it doesnt read the 40 pixels that are used for line detection in the middle
 	
     int rightError = 0;
-    for(int i = 0; i < TotalRightSidePixels; i++) {
+    for(int i = 0; i < TotalRightSidePixels; i++) 
 		rightError += ( get_pixel(CAMERA_HEIGHT/2, TotalRightSidePixels-i, 3) > threshold) ? i : 0; // Be warned this is right from the ROBOTS perspective
-    		set_pixel(CAMERA_HEIGHT/2, TotalRightSidePixels-i, 255, 0,0);
-	}
-
+		
 	return rightError;
 }
 
 int getLeftSideErrorSignal() {
-    int TotalLeftSidePixels = (int)(CAMERA_WIDTH/2)-40; //The -40 is so it doesnt read the 40 pixels that are used for line detection in the middle
+    const int TotalLeftSidePixels = (int)(CAMERA_WIDTH/2)-40; //The -40 is so it doesnt read the 40 pixels that are used for line detection in the middle
 	
 	int leftError = 0;
-	for(int i = 0; i < TotalLeftSidePixels; i++) {
+	for(int i = 0; i < TotalLeftSidePixels; i++) 
 		leftError += ( get_pixel(CAMERA_HEIGHT/2, i+TotalLeftSidePixels+80, 3) > threshold ) ? i : 0; //+TotalSidePixels is for skipping what we already put into Right[] above. +80 is to skip the middle p$
-		set_pixel(CAMERA_HEIGHT/2, i+TotalLeftSidePixels+80, 255, 0, 0);
-	}
-
+		
 	return leftError;
 }
 
 
 int main() {
-        signal(2, betterstop);
 	init();
+	signal(2, betterstop);
 	
 	connect_to_server("130.195.6.196", 1024);
 	
@@ -94,26 +90,27 @@ int main() {
 				double rightError = ((double)getRightSideErrorSignal());
 				double leftError  = ((double)getLeftSideErrorSignal()); 
 
-
-				
-
-				if(!isOnLine(CAMERA_HEIGHT/2) && rightError <= 0 && leftError <= 0) {
-						if(left_velocity < right_velocity) { 
+				if(!isOnLine(CAMERA_HEIGHT/2) && rightError <= 0 && leftError <= 0) { // If we're off the line and cant see the line
+						cout << "Lost line" << endl;
+						cout << "\tTurning " << flush;
+						if(left_velocity < right_velocity) { //If we were last turning left
+							cout << "Left to find it." << endl;
 							set_motor(LEFT_MOTOR, -60);
 							set_motor(RIGHT_MOTOR, -30);
 							sleep1(0,2000);
-						} else { 
+						} else { // If we were last turning right
+							cout << "Right to find it." << endl;
 							set_motor(LEFT_MOTOR, -30);
 							set_motor(RIGHT_MOTOR, -60);
 							sleep1(0,2000);
 						}
-					continue;
+					continue; // Skip the rest of the instructions in this loop
 				}
 
-				if(rightError > 3500 && leftError > 3500 && isOnLine(CAMERA_HEIGHT/2+40)) {
+				if(rightError > 3500 && leftError > 3500 && isOnLine(CAMERA_HEIGHT/2+40)) { 
 					set_motor(RIGHT_MOTOR, 60);
 					set_motor(LEFT_MOTOR, 20);
-					cout << "\nJunction" << endl;
+					
 					sleep1(0,500000);
 					continue;
 				}
@@ -122,8 +119,8 @@ int main() {
 				double proportional_signal = error_signal*kP;
 
 				int final_signal = proportional_signal;
-				right_velocity = 100+final_signal;
-				left_velocity = 100-1*final_signal;
+				right_velocity = 80+final_signal;
+				left_velocity = 80-1*final_signal;
 
 				set_motor(RIGHT_MOTOR, right_velocity);
 				set_motor(LEFT_MOTOR, left_velocity);   
