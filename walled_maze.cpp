525d48a88759bde1419db02f04bbfbf5ea0a291c
Alex Lee <alexgrlee@gmail.com>
2017-05-23 14:49:15 +1200

    More normal constants

--- walled_maze.cpp
+++ walled_maze.cpp
@@ -11,10 +11,10 @@ const int RIGHT_SENSOR = 1;
 const int LEFT_MOTOR = 1;
 const int RIGHT_MOTOR = 2;
 
-const int DEF_SPD = 40; // Default speed for motors
+const int DEF_SPD = 50; // Default speed for motors
 const int THRESHOLD = 310; // Threshold value for proximity to left wall - Change with testing.
 
-double mazeFactor = 0.30; // To be changed with testing.
+double mazeFactor = 0.165; // To be changed with testing.
 
 bool mazeDone = false;
 
