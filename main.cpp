67a6463d4c7fc74d7fb1e46d35f03534b1f51941
NHAS <jordanatararimu@gmail.com>
2017-05-25 02:55:52 +0000

    I dont know how this works

--- main.cpp
+++ main.cpp
@@ -80,6 +80,20 @@ int getLeftSideErrorSignal() {
 }
 
 
+bool detectJunction() {
+
+	int Lerr = 0;
+	int Rerr = 0;
+
+	for(int i = 0; i < 60; i++) 
+		Lerr += (get_pixel(CAMERA_HEIGHT/2+70-i, CAMERA_WIDTH/2-60, 3) > 120) ? i : 0;
+
+	for(int i = 0; i < 60; i++) 
+		Rerr += (get_pixel(CAMERA_HEIGHT/2+70-i, CAMERA_WIDTH/2+60, 3) > 120) ? i : 0; 
+
+	return (Lerr > 1650) && (Rerr > 1650);
+
+}
 
 //////////////////////////////// Helper functions ///////////////////////////////////////
 
@@ -111,6 +125,7 @@ void QuandrantTwo() {
 	cout << "Running quandrant 2..." << flush;
 	int left_velocity = 0;
 	int right_velocity = 0;
+	const double kP = 0.011;
 	while(true) {
 		take_picture();
 				double rightError = ((double)getRightSideErrorSignal());
@@ -157,7 +172,8 @@ void QuandrantThree() {
 
 
 	const double kP = 0.011;
- 
+	int left_velocity = 0;
+	int right_velocity = 0; 
 	
 	while(true) {
 		take_picture();
@@ -165,49 +181,47 @@ void QuandrantThree() {
 				const int TotalSidePixels = (int)(CAMERA_WIDTH/2)-40; //The -40 is so it doesnt read the 40 pixels that are used for line detection in the middle
 	
 				double rightError = 0;
-				int threshold = getThresholdHorizontal(CAMERA_HEIGHT/2, TotalRightSidePixels, TotalRightSidePixels);
-				for(int i = 0; i < TotalRightSidePixels; i++) 
-					rightError += ( get_pixel(CAMERA_HEIGHT/2, TotalRightSidePixels-i, 3) > threshold) ? i : 0; // Be warned this is right from the ROBOTS perspective
+				int localthreshold = 120;//getThresholdHorizontal(CAMERA_HEIGHT/2, TotalSidePixels, TotalSidePixels);
+				for(int i = 0; i < TotalSidePixels; i++) 
+					rightError += ( get_pixel(CAMERA_HEIGHT/2, TotalSidePixels-i, 3) > localthreshold) ? i : 0; // Be warned this is right from the ROBOTS perspective
 
 				double leftError = 0;
-				int threshold = getThresholdHorizontal(CAMERA_HEIGHT/2, TotalRightSidePixels+80, TotalRightSidePixels);
-				for(int i = 0; i < TotalLeftSidePixels; i++) 
-					leftError += ( get_pixel(CAMERA_HEIGHT/2, i+TotalLeftSidePixels+80, 3) > threshold ) ? i : 0; //+TotalSidePixels is for skipping what we already put into Right[] above. +80 is to skip the middle 
+				localthreshold = 120;//getThresholdHorizontal(CAMERA_HEIGHT/2, TotalSidePixels+80, TotalSidePixels);
+				for(int i = 0; i < TotalSidePixels; i++) 
+					leftError += ( get_pixel(CAMERA_HEIGHT/2, i+TotalSidePixels+80, 3) > localthreshold ) ? i : 0; //+TotalSidePixels is for skipping what we already put into Right[] above. +80 is to skip the middle 
 
-				if(!isOnLine(CAMERA_HEIGHT/2) && rightError <= 0 && leftError <= 0) { // If we're off the line and cant see the line
-						
-						if(left_velocity < right_velocity) { //If we were last turning left
-							
-							set_motor(LEFT_MOTOR, -60);
-							set_motor(RIGHT_MOTOR, -30);
-							sleep1(0,2000);
-						} else { // If we were last turning right
-							
-							set_motor(LEFT_MOTOR, -30);
-							set_motor(RIGHT_MOTOR, -60);
-							sleep1(0,2000);
-						}
-					continue; // Skip the rest of the instructions in this loop
-				}
 
-				if(rightError > 1500 && leftError > 1500) {// If both are high values then its most probably a junction
-					set_motor(RIGHT_MOTOR, 50);
-					set_motor(LEFT_MOTOR, 50);
-					sleep1(0, 200);
-					
-					set_motor(RIGHT_MOTOR, 50);
-					set_motor(LEFT_MOTOR, -50);
-					sleep1(0, 200);
-					
+				if(detectJunction()) {
+					set_motor(LEFT_MOTOR, -60);
+                                        set_motor(RIGHT_MOTOR, -30);
+                                        sleep1(0,3000);
+					cout << "Junction" << endl;
 					continue;
 				}
-				
+
+				if(!isOnLine(CAMERA_HEIGHT/2) && rightError <= 0 && leftError <= 0) { // If we're off the line and cant see the line
+					 if(left_velocity < right_velocity) { //If we were last turning left
+                                                        
+                                                        set_motor(LEFT_MOTOR, -60);
+                                                        set_motor(RIGHT_MOTOR, -30);
+                                                        sleep1(0,2500);
+                                                } else { // If we were last turning right
+                                                        
+                                                        set_motor(LEFT_MOTOR, -30);
+                                                        set_motor(RIGHT_MOTOR, -60);
+                                                        sleep1(0,2500);
+                                                }
+
+					continue;
+				}
+
+
 				double error_signal = (rightError - leftError);
 				double proportional_signal = error_signal*kP;
 
 				int final_signal = proportional_signal;
-				right_velocity = 60+final_signal;
-				left_velocity = 60-1*final_signal;
+				right_velocity = 80+final_signal;
+				left_velocity = 80-1*final_signal;
 
 				set_motor(RIGHT_MOTOR, right_velocity);
 				set_motor(LEFT_MOTOR, left_velocity);   
@@ -228,7 +242,7 @@ int main() {
 	init();
 	
 //	QuandrantOne();
-//	QuandrantTwo();
+	QuandrantTwo();
 	QuandrantThree();
 	QuandrantFour();
 
